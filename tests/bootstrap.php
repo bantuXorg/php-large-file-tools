b9cb4a1a27041b8009c1c03f8b3c7d7369c20d64
Andreas Fischer <af@bantuX.org>
2015-02-28 03:18:05 +0100

    Add PHPUnit.

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+* (c) Andreas Fischer <af@bantuX.org>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+error_reporting(E_ALL);
+date_default_timezone_set('UTC');
