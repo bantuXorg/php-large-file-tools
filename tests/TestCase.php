e2eb8c15ca6d98ddabf027422f6b092ccddd2dff
Andreas Fischer <af@bantuX.org>
2015-03-18 01:25:41 +0100

    Add test case testing filesize() against environment.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,6 +11,39 @@ namespace bantu\LargeFileTools;
 
 abstract class TestCase extends \PHPUnit_Framework_TestCase
 {
+    protected $tempFilesToUnlinkOnTearDown = array();
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        foreach ($this->tempFilesToUnlinkOnTearDown as $filename) {
+            exec(sprintf(
+                'rm -f %s',
+                escapeshellarg($filename)
+            ));
+        }
+    }
+
+    /**
+    * Creates a file containing all zero bytes and returns its path.
+    *
+    * @param string $sizeString
+    *
+    * @return string
+    */
+    protected function createZeroFile($sizeString)
+    {
+        $filename = tempnam(sys_get_temp_dir(), 'lft-test-');
+        $this->assertTrue(file_exists($filename));
+        exec(sprintf(
+            'truncate --size %s %s',
+            escapeshellarg($sizeString),
+            escapeshellarg($filename)
+        ));
+        $this->tempFilesToUnlinkOnTearDown[] = $filename;
+        return $filename;
+    }
+
     public function getEnv($varname)
     {
         $result = getenv($varname);
--- tests/TravisEnvironmentTest.php
+++ tests/TravisEnvironmentTest.php
@@ -15,4 +15,20 @@ class TravisEnvironmentTest extends TestCase
     {
         $this->assertSame($this->getEnv('phpint') / 8, PHP_INT_SIZE);
     }
+
+    public function testLargeFileSupport()
+    {
+        if ($this->getEnv('phpint') == 64) {
+            // 5G = 5 * 2^30 = 5368709120
+            $expected = 5368709120;
+        } else if ($this->getEnv('phplfs')) {
+            // 5G = 1G = 2^30 = 1073741824 (mod 2^32)
+            $expected = 1073741824;
+        } else {
+            $expected = false;
+        }
+        $filename = $this->createZeroFile('5G');
+        $actual = filesize($filename);
+        $this->assertSame($expected, $actual);
+    }
 }
