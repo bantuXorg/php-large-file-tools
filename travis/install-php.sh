61e9ffc14bfcbd9dc4babee15954ef3422054f83
Andreas Fischer <af@bantuX.org>
2015-05-02 14:45:42 +0200

    Do not reconfigure / recompile / reinstall PHP build.

--- travis/install-php.sh
+++ travis/install-php.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -ex
+
+genid() {
+  echo -n "$1" | md5sum | cut -d ' ' -f 1
+}
+
 export CCASFLAGS="-m$phpint"
 export CPPFLAGS="-m$phpint"
 export LDFLAGS="-m$phpint"
@@ -18,14 +23,20 @@ then
   curl -L "http://php.net/get/php-$phpver.tar.bz2/from/this/mirror" |
     tar xj --strip-components=1
 fi
-./configure \
-  --disable-cgi \
-  --prefix="$(pwd)/target" \
-  --without-pear \
-  --with-curl \
-  > /dev/null
-make > /dev/null
-make install
+php_configure_args="
+  --disable-cgi
+  --prefix=$(pwd)/target
+  --without-pear
+  --with-curl
+"
+php_build_id=$(genid "$php_configure_args")
+if [[ ! -e './.bn2buildid' || "$(cat './.bn2buildid')" != "$php_build_id" ]]
+then
+  ./configure $php_configure_args > /dev/null
+  make > /dev/null
+  make install
+  echo -n "$php_build_id" > ./.bn2buildid
+fi
 popd
 
 # Xdebug
@@ -36,11 +47,17 @@ then
   curl -L "http://xdebug.org/files/xdebug-$phpxdebugver.tgz" |
     tar xz --strip-components=1
 fi
-"../$phptarget/bin/phpize"
-./configure \
-  --enable-xdebug \
-  --with-php-config="../$phptarget/bin/php-config" \
-  > /dev/null
-make > /dev/null
-make install
+xdebug_configure_args="
+  --enable-xdebug
+  --with-php-config=../$phptarget/bin/php-config
+"
+xdebug_build_id=$(genid "$xdebug_configure_args")
+if [[ ! -e './.bn2buildid' || "$(cat './.bn2buildid')" != "$xdebug_build_id" ]]
+then
+  "../$phptarget/bin/phpize"
+  ./configure $xdebug_configure_args > /dev/null
+  make > /dev/null
+  make install
+  echo -n "$xdebug_build_id" > ./.bn2buildid
+fi
 popd
